036249978be376a791d47831bc978d4473832893
kubamarchwicki <kuba@marchwicki.pl>
2016-03-07 21:55:14 +0100

    Choose styles bases on attrbute :odt-styles:
    - default to 'asciidoctor'

--- slim/fodt/helpers.rb
+++ slim/fodt/helpers.rb
@@ -1,2 +1,21 @@
 require 'time'
 Asciidoctor::DEFAULT_EXTENSIONS['fodt'] = '.fodt'
+
+module Slim::Helpers
+
+  def include_styles
+    if attr? 'odt-styles'
+      style = attr 'odt-styles'
+    else
+      style = 'asciidoctor'
+    end
+
+    (document::options[:template_dirs] || []).each do |folder|
+      filename = "#{folder}/#{document::backend}/_styles-#{style}.fodt.slim"
+      if File.exists?(filename)
+      	return Slim::Template.new(filename).render
+      end
+    end 
+  end
+
+end
